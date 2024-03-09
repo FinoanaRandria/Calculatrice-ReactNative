646657ea08d89c0a408728b8ac62762869d6a122
finoana <110087713+FinoanaRandria@users.noreply.github.com>
2024-03-09 20:18:41 +0300

    dark mod

--- App.jsx
+++ App.jsx
@@ -1,18 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const colors = {
+    dark: "#22252D",
+    dark1: "#292B36",
+    dark2: "#272B33",
+    light: "#FFF",
+    light1: "#F1F1F1",
+    light2: "#F7F7F7",
+  };
+
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        height: "100%",
+        width: "100%",
+        paddingVertical: 20,
+        backgroundColor: darkTheme ? colors.dark : colors.light,
+      }}
+    >
       <StatusBar style="auto" />
+      <Switch
+        value={darkTheme}
+        onValueChange={() => setDarkTheme(!darkTheme)}
+        thumbColor={darkTheme ? colors.dark : colors.light}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    
-  },
-});
